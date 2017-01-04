2bdb58622eecc3ee0d610c6ee0a8bcb7d94661db
Walter Manger <walter.manger@gmail.com>
2017-01-03 17:13:41 -0700

    More Yakking
    Removing the need for the settings folder

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,17 +5,32 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(display-time-day-and-date t)
  '(display-time-mode t)
  '(electric-indent-mode nil)
  '(fci-rule-color "#373b41")
  '(flycheck-disabled-checkers (quote (javascript-jshint)))
  '(flycheck-javascript-jscs-executable "/usr/local/bin/jscs")
- '(js2-basic-offset 2)
+ '(js-indent-level 2)
  '(js2-include-browser-externs nil)
  '(js2-include-jslint-globals nil)
  '(js2-mode-show-parse-errors nil)
  '(js2-mode-show-strict-warnings nil)
  '(js2-strict-trailing-comma-warning nil)
+ '(package-selected-packages
+   (quote
+    (babel-repl jade tern-auto-complete tern go-guru yasnippet yaml-mode which-key web-mode wakatime-mode w3m use-package twittering-mode try swiper-helm spaceline smartparens smart-mode-line-powerline-theme sass-mode restclient rainbow-delimiters racket-mode paredit org-journal org-bullets multi-term markdown-mode magit-filenotify helm-projectile helm-dash helm-ag harvest gotest go-eldoc go-autocomplete git-gutter flycheck evil-surround evil-magit emms edit-server company color-theme-solarized color-theme-sanityinc-solarized ac-js2)))
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-code ((t (:family "Hack"))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+ '(org-table ((t (:family "Hack")))))
--- .emacs.d/settings/setup-smartparens.el
+++
@@ -1,8 +0,0 @@
-;;; setup-smartparens --- Summary
-;;; Commentary:
-;;; Simple beginnings, but will probably need some major modifications.
-;;; Code:
-(smartparens-global-mode 1)
-
-(provide 'setup-smartparens)
-;;; setup-smartparens.el ends here
