3c85a561ce705ac1088240803059dfc3fcb5ff09
Walter Manger <walter.manger@gmail.com>
2017-02-03 11:11:04 -0700

    Moving back to solarized

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "#073642" :foreground "#839496" :box (:line-width 1 :color "#073642" :style unspecified) :overline "#073642" :underline nil))))
  '(org-code ((t (:family "Hack"))))
  '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
